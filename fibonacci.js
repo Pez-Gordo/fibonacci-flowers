026b0d73bde1ea3d134d14844d7142919d6b5f08
pez <emvenegas@gmail.com>
2021-03-23 23:03:00 +0000

    exercise finished

--- fibonacci.js
+++ fibonacci.js
@@ -2,33 +2,36 @@ const canvas = document.getElementById('canvas1')
 const ctx = canvas.getContext('2d')
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
+ctx.globalCompositeOperation = 'destination-over'
+hue = Math.random() * 360
 
-let size = 0
-let positionX = 0
-let positionY = 0
-let angle = 0
+let number = 0
+let scale = 10
 
 function drawFlower() {
-    ctx.fillStyle = 'red'
-    ctx.strokeStyle = 'blue'
-    ctx.lineWidth = 5
+    let angle = number * 0.09
+    let radius = scale * Math.sqrt(number)
+    let positionX = radius * Math.sin(angle) + canvas.width / 2
+    let positionY = radius * Math.cos(angle) + canvas.height / 2
+    
+    ctx.fillStyle = 'hsl(' + hue + ', 100%, 50%)'
+    ctx.strokeStyle = 'black'
+    ctx.lineWidth = 4
     ctx.beginPath()
-    ctx.arc(positionX, positionY, size, 0, Math.PI * 2)
+    ctx.arc(positionX, positionY, 8, 0, Math.PI * 2)
     ctx.closePath()
     ctx.fill()
     ctx.stroke()
+
+    number ++
+    hue += 0.5
 }
 
 function animate() {
     // draw each frame
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-
-    size += 0.15
-    positionX += 0.2
-    positionY += 0.2
-
+    //ctx.clearRect(0, 0, canvas.width, canvas.height)
     drawFlower()
-    
+    if(number > 950) return
     requestAnimationFrame(animate)
 }
 animate()
