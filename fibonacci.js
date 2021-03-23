f1dac40618660877859f2d26c4efca3e267679a3
pez <emvenegas@gmail.com>
2021-03-23 22:17:12 +0000

    basic canvas setup finished

---
+++ fibonacci.js
@@ -0,0 +1,34 @@
+const canvas = document.getElementById('canvas1')
+const ctx = canvas.getContext('2d')
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+let size = 0
+let positionX = 0
+let positionY = 0
+let angle = 0
+
+function drawFlower() {
+    ctx.fillStyle = 'red'
+    ctx.strokeStyle = 'blue'
+    ctx.lineWidth = 5
+    ctx.beginPath()
+    ctx.arc(positionX, positionY, size, 0, Math.PI * 2)
+    ctx.closePath()
+    ctx.fill()
+    ctx.stroke()
+}
+
+function animate() {
+    // draw each frame
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+    size += 0.15
+    positionX += 0.2
+    positionY += 0.2
+
+    drawFlower()
+    
+    requestAnimationFrame(animate)
+}
+animate()
